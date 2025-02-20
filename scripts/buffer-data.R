3440fe9d2280036b14b5f18ca1947559c3a83e29
Lukáš Hejtmánek <hejtmy@gmail.com>
2025-02-20 17:34:19 +0100

    Data buffering

--- scripts/buffer-data.R
+++ scripts/buffer-data.R
@@ -5,14 +5,14 @@ dir.create("temp/data", showWarnings = FALSE, recursive = TRUE)
 
 drive_auth()
 file_id <- as_id("1pgQyD39k5KPln4wkyJXhH5215Krmq5Mb")
-drive_download(file = file_id, path = "temp/results_survey_vistateleport.csv")
+drive_download(file = file_id, path = file.path("temp", "results_survey_vistateleport.csv"))
 
 # Load the data
 positions_sheet <- "18mrx8kSpGEbWkHkKVoYaUkAHyi1bZZVgaL7mDlRHkbc"
 positions <- read_sheet(positions_sheet)
 positions <- positions[, 1:5]
-write.csv(positions, "temp/positions.csv", row.names = FALSE)
+write.csv(positions, file.path("temp", "positions.csv"), row.names = FALSE)
 
 data_id <-  as_id("1K2uInHO3pnEJv3xZQnza_Jas2tYk9468")
-drive_download(file = data_id, path = "temp/data.zip")
-unzip("temp/data.zip", exdir = "temp/data")
+drive_download(file = data_id, path = file.path("temp", "data.zip"))
+unzip(file.path("temp", "data.zip"), exdir = file.path("temp", "data"))
